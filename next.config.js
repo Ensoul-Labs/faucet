4b953e9fddf348a7554fcd44b498afc6c26f28d0
tingfei <1228959039@qq.com>
2022-12-18 14:33:28 +0800

    add style

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'storage.googleapis.com',
+      }
+    ]
+  },
   reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `https://us-central1-ensoul-labs-df8ed.cloudfunctions.net/app/:path*`,
+        },
+      ],
+    }
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,41 @@
+import { WagmiConfig, createClient, configureChains } from "wagmi"
+import { mainnet, polygon } from '@wagmi/chains';
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Home from ".";
+import '@rainbow-me/rainbowkit/styles.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+export const { chains, provider } = configureChains(
+  // [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [polygon],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
+const client = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+})
+
+function MyApp() {
+  return (
+    <WagmiConfig client={client}>
+      <RainbowKitProvider chains={chains}>
+        <Home />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
 }
+
+export default MyApp
